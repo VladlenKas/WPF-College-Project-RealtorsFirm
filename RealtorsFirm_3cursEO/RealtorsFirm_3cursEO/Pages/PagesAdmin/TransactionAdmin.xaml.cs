cf1048f45271ed65d000f3e9237d6b93b6a29389
Vladlen Kasimov <vladlenkas06@gmail.com>
2024-12-19 03:34:20 +0200

    Свойства, интерфейс на странице с транзакцией
    - Добавлены свойства для отслеживания изменения, таких как цена, цена со скидкой, бонусы, списываемые бонусы, количество бонусов у клиента и свойства для выбираемых элементов
    - Добавлена логика, при которой доступность оформления кнопки транзакции доступна только после выбора всех полей
    - Изменен стиль: теперь выпадающий список с клиентами не меняет высоту в разметке, а перекрывает другие элементы

--- RealtorsFirm_3cursEO/RealtorsFirm_3cursEO/Pages/PagesAdmin/TransactionAdmin.xaml.cs
+++ RealtorsFirm_3cursEO/RealtorsFirm_3cursEO/Pages/PagesAdmin/TransactionAdmin.xaml.cs
@@ -25,22 +25,111 @@ namespace RealtorsFirm_3cursEO.Pages.PagesAdmin
     /// </summary>
     public partial class TransactionAdmin : Page, INotifyPropertyChanged
     {
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            VisibilityButtonAdd();
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        #region Свойства и поля
         /// <summary>
         /// Приватное поле для хранения значения с общей стоимостью услуг
         /// </summary>
-        private int _amountPrices;
+        private int _amountTotal;
         /// <summary>
         /// Свойство для доступа к полю с общей стоимостью услуг
         /// </summary>
-        public int AmountPrices
+        public int AmountTotal
         {
-            get { return _amountPrices; }
+            get { return _amountTotal; }
             set
             {
-                if (_amountPrices != value)
+                if (_amountTotal != value)
                 {
-                    _amountPrices = value;
-                    OnPropertyChanged(nameof(AmountPrices));
+                    _amountTotal = value;
+                    CountBonuses = _amountTotal / 10;
+                    OnPropertyChanged(nameof(AmountTotal));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Приватное поле для хранения значения с общей стоимостью услуг со скидкой
+        /// </summary>
+        private int _amountDiscard;
+        /// <summary>
+        /// Свойство для доступа к полю с общей стоимостью услуг со скидкой
+        /// </summary>
+        public int AmountDiscard
+        {
+            get { return _amountDiscard; }
+            set
+            {
+                if (_amountDiscard != value)
+                {
+                    _amountDiscard = value;
+                    OnPropertyChanged(nameof(AmountDiscard));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Приватное поле для хранения значения с количеством начисляемых бонусов
+        /// </summary>
+        private int _countBonuses;
+        /// <summary>
+        /// Свойство для доступа к полю с количеством начисляемых бонусов
+        /// </summary>
+        public int CountBonuses
+        {
+            get { return _countBonuses; }
+            set
+            {
+                if (_countBonuses != value)
+                {
+                    _countBonuses = value;
+                    OnPropertyChanged(nameof(CountBonuses));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Приватное поле для хранения значения с количеством списываемых бонусов у выбранного клиента
+        /// </summary>
+        private int _bonusesDiscardSelectedClient;
+        /// <summary>
+        /// Свойство для доступа к полю с количеством списываемых бонусов у выбранного клиента
+        /// </summary>
+        public int BonusesDiscardSelectedClient
+        {
+            get { return _bonusesDiscardSelectedClient; }
+            set
+            {
+                if (_bonusesDiscardSelectedClient != value)
+                {
+                    _bonusesDiscardSelectedClient = value;
+                    OnPropertyChanged(nameof(BonusesDiscardSelectedClient));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Приватное поле для хранения значения с количеством бонусов у выбранного клиента
+        /// </summary>
+        private int _bonusesSelectedClient;
+        /// <summary>
+        /// Свойство для доступа к полю с количеством бонусов у выбранного клиента
+        /// </summary>
+        public int BonusesSelectedClient
+        {
+            get { return _bonusesSelectedClient; }
+            set
+            {
+                if (_bonusesSelectedClient != value)
+                {
+                    _bonusesSelectedClient = value;
+                    OnPropertyChanged(nameof(BonusesSelectedClient));
                 }
             }
         }
@@ -62,7 +151,7 @@ namespace RealtorsFirm_3cursEO.Pages.PagesAdmin
 
                 if (_selectedClient is Client client)
                 {
-                    List<Estate> estates = new (App.Context.Estates.Where(r => r.IdClient == client.IdClient).ToList());
+                    List<Estate> estates = new(App.Context.Estates.Where(r => r.IdClient == client.IdClient).ToList());
                     if (estates.Count == 0)
                     {
                         var noDataItem = new[] { new { Address = "У данного клиента нет недвижимостей" } };
@@ -87,12 +176,115 @@ namespace RealtorsFirm_3cursEO.Pages.PagesAdmin
             }
         }
 
+        /// <summary>
+        /// Приватное поле для хранения значения с выбранным статусом транзакции
+        /// </summary>
+        private StatusTransaction _selectedStatusTransaction;
+        /// <summary>
+        /// Свойство для доступа к полю с выбранным статусом транзакции
+        /// </summary>
+        public StatusTransaction SelectedStatusTransaction
+        {
+            get { return _selectedStatusTransaction; }
+            set
+            {
+                if (_selectedStatusTransaction != value)
+                {
+                    _selectedStatusTransaction = value;
+                    OnPropertyChanged(nameof(SelectedStatusTransaction));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Приватное поле для хранения значения с выбранной недвижимостью
+        /// </summary>
+        private Estate _selectedEstate;
+        /// <summary>
+        /// Свойство для доступа к полю с выбранной недвижимостью
+        /// </summary>
+        public Estate SelectedEstate
+        {
+            get { return _selectedEstate; }
+            set
+            {
+                if (_selectedEstate != value)
+                {
+                    _selectedEstate = value;
+                    OnPropertyChanged(nameof(SelectedEstate));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Приватное поле для хранения выбранных услуг
+        /// </summary>
+        private List<Price> _selectedPrices = new List<Price>();
+        /// <summary>
+        /// Свойство для доступа к полю с выбранными услугами
+        /// </summary>
+        public List<Price> SelectedPrices
+        {
+            get { return _selectedPrices; }
+            set
+            {
+                if (_selectedPrices != value)
+                {
+                    _selectedPrices = value;
+                    OnPropertyChanged(nameof(SelectedPrices));
+                }
+            }
+        }
+
+        #endregion
+
         public TransactionAdmin(Employee employee)
         {
             InitializeComponent();
 
-            amountPricesTextBlock.DataContext = this;
+            DataContext = this;
             UserFio.Text = $"{employee.FullName}";
+        }
+
+        private void VisibilityButtonAdd()
+        {
+            // Удаляем событие для того, чтобы кнопка были недоступна до проверки
+            CreateNewTransatcionButton.Click -= CreateNewTransatcion_Click;
+            // Делаем кнопку полупрозрачной для демонстрации некликабельности
+            CreateNewTransatcionButton.Opacity = 0.5;
+
+            // Проверяем, заполнены ли все необходимые поля
+            bool allFieldsFilled = CheckFields();
+
+            if (allFieldsFilled)
+            {
+                // Подключаем обработчик, если все поля заполнены
+                CreateNewTransatcionButton.Click += CreateNewTransatcion_Click;
+                CreateNewTransatcionButton.Opacity = 1;
+            }
+        }
+
+        private bool CheckFields()
+        {
+            bool allFieldsFilled = false;
+
+            if (WriteOffButton.IsChecked == true)
+            {
+                allFieldsFilled = SelectedClient != null &&
+                                           SelectedEstate != null &&
+                                           SelectedStatusTransaction != null &&
+                                           SelectedPrices.Count != 0; 
+            }
+            else if (ChargeBonusesButton.IsChecked == true)
+            {
+                allFieldsFilled = SelectedClient != null &&
+                                           SelectedEstate != null &&
+                                           SelectedStatusTransaction != null &&
+                                           SelectedPrices.Count != 0 &&
+                                           amountBonusesMinusTextBox.Text != string.Empty;
+            }
+
+            return allFieldsFilled;
         }
 
         /// <summary>
@@ -106,7 +298,15 @@ namespace RealtorsFirm_3cursEO.Pages.PagesAdmin
                 if (!selectedPricesListView.Items.Contains(price))
                 {
                     selectedPricesListView.Items.Add(price);
-                    AmountPrices += price.Cost;
+                    AmountTotal += price.Cost;
+                    AmountDiscard += price.Cost;
+
+                    var lisrPrices = new List<Price>();
+                    foreach (var selectedPrice in selectedPricesListView.Items)
+                    {
+                        lisrPrices.Add(price);
+                    }
+                    SelectedPrices = new List<Price>(lisrPrices);
                 }
                 PricesSearch.Text = string.Empty;
             }
@@ -121,12 +321,10 @@ namespace RealtorsFirm_3cursEO.Pages.PagesAdmin
             if (selectedItem is Client client)
             {
                 SelectedClient = client;
-                MessageBox.Show("Вы добавили клиента");
             }
             else if (selectedItem is null)
             {
                 SelectedClient = null;
-                MessageBox.Show("Вы удалили клиента");
             }
         }
 
@@ -143,10 +341,116 @@ namespace RealtorsFirm_3cursEO.Pages.PagesAdmin
             SelectedClient = null;
         }
 
-        protected virtual void OnPropertyChanged(string propertyName)
+        private void AmountBonusesMinusTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            DataValidations.ValidateInputNumbers(e);
         }
-        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void AmountBonusesMinusTextBox_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            DataValidations.ValidatePasteNumbers(e);
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            RadioButton button = sender as RadioButton;
+
+            if (button.Content.ToString() == "Списать")
+            {
+                if (SelectedClient == null)
+                {
+                    WriteOffButton.IsChecked = true;
+                    ChargeBonusesButton.IsChecked = false;
+
+                    MessageBox.Show("Сначала выберите клиента!",
+                        "Предупреждение",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Warning);
+                }
+                else if (SelectedClient != null && SelectedClient.Bonuses == 0)
+                {
+                    WriteOffButton.IsChecked = true;
+                    ChargeBonusesButton.IsChecked = false;
+
+                    MessageBox.Show("У данного клиента нет бонусов.",
+                        "Предупреждение",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Warning);
+                }
+                else
+                {
+                    int bonuses = Convert.ToInt32(SelectedClient.Bonuses);
+                    BonusesSelectedClient = bonuses;
+                    AmountTotal = AmountDiscard - BonusesDiscardSelectedClient;
+                }
+            }
+            else
+            {
+                AmountTotal = AmountDiscard;
+            }
+            OnPropertyChanged("");
+        }
+
+        private void ClearPricesListView_Click(object sender, RoutedEventArgs e)
+        {
+            selectedPricesListView.Items.Clear();
+            SelectedPrices = new List<Price>();
+            AmountTotal = 0;
+        }
+
+        private void AmountBonusesMinusTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox.Text != string.Empty)
+            {
+                int minusBonuses = int.Parse(textBox.Text);
+
+                if (BonusesSelectedClient >= minusBonuses)
+                {
+                    AmountTotal = AmountDiscard;
+
+                    BonusesDiscardSelectedClient = minusBonuses;
+                    AmountTotal = AmountDiscard - BonusesDiscardSelectedClient;
+                }
+                else
+                {
+                    amountBonusesMinusTextBox.TextChanged -= AmountBonusesMinusTextBox_TextChanged;
+                    textBox.Text = BonusesDiscardSelectedClient.ToString();
+                    textBox.SelectionStart = textBox.Text.Length;
+                    amountBonusesMinusTextBox.TextChanged += AmountBonusesMinusTextBox_TextChanged;
+
+                    MessageBox.Show("Вы не можете списать бонусов больше, чем есть у клиента.",
+                            "Предупреждение",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Warning);
+                }
+            }
+            else
+            {
+                AmountTotal = AmountDiscard;
+            }
+        }
+
+        private void AllBonusesSelect_Click(object sender, RoutedEventArgs e)
+        {
+            amountBonusesMinusTextBox.Text = SelectedClient.Bonuses.ToString();
+        }
+
+        private void CreateNewTransatcion_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EstateComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            SelectedEstate = comboBox.SelectedItem as Estate;
+        }
+
+        private void StatusComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            SelectedStatusTransaction = comboBox.SelectedItem as StatusTransaction;
+        }
     }
 }
